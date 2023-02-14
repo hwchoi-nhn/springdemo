657e35fe0f389faf82950cdb9d524e0d7e739181
최현욱/클라우드운영팀/EP <hyunwook.choi@nhnenter.com>
2023-02-14 18:01:38 +0900

    Spring Init

---
+++ src/test/java/com/nhnenter/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnenter.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
