442e4fd9930af7b1a6f0baad3c65d41ec18fb2e1
최현욱/클라우드운영팀/EP <hyunwook.choi@nhnenter.com>
2023-02-15 01:49:00 +0900

    SampleController.java edit

--- src/main/java/com/nhnenter/springdemo/controller/SampleController.java
+++ src/main/java/com/nhnenter/springdemo/controller/SampleController.java
@@ -6,5 +6,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+    @GetMapping("/")
+    public String root() {
+        return "Spring Project";
+    }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
